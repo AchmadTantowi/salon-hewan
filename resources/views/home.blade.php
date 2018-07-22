712b114208d28c2eb380145654f22e85d60fb1a3
Achmad Tantowi <achmad.tantowi@edutren.com>
2018-07-22 12:00:08 +0700

    update testomoni

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -101,7 +101,7 @@
 						<div id="recommended-item-carousel" class="carousel slide" data-ride="carousel">
 							<div class="carousel-inner">
 							@foreach($testimonis as $testimoni)
-								<div class="item active">	
+								<div class="item {{ $loop->first ? 'active' : '' }}">	
 									<div class="col-sm-12" style="padding-left:100px;padding-right:100px;">
 										<div class="media commnets" style="padding-left:100px;padding-right:100px;">
 										<a class="pull-left" href="#">
@@ -115,15 +115,43 @@
 									</div>
 								</div>
 							@endforeach
+							{{-- <div class="item active">	
+								<div class="col-sm-12" style="padding-left:100px;padding-right:100px;">
+									<div class="media commnets" style="padding-left:100px;padding-right:100px;">
+									<a class="pull-left" href="#">
+										<img class="media-object" src="{{ asset('assets/frontend/images/blog/man-one.jpg') }}" alt="">
+									</a>
+									<div class="media-body">
+										<h4 class="media-heading">oio</h4>
+										<p>ooi</p>
+									</div>
+									</div><!--Comments-->
+								</div>
 							</div>
-							 <!-- <a class="left recommended-item-control" href="#recommended-item-carousel" data-slide="prev">
+							<div class="item">	
+								<div class="col-sm-12" style="padding-left:100px;padding-right:100px;">
+									<div class="media commnets" style="padding-left:100px;padding-right:100px;">
+									<a class="pull-left" href="#">
+										<img class="media-object" src="{{ asset('assets/frontend/images/blog/man-one.jpg') }}" alt="">
+									</a>
+									<div class="media-body">
+										<h4 class="media-heading">ppp</h4>
+										<p>ooi</p>
+									</div>
+									</div><!--Comments-->
+								</div>
+							</div> --}}
+							</div>
+							 <a class="left recommended-item-control" href="#recommended-item-carousel" data-slide="prev">
 								<i class="fa fa-angle-left"></i>
 							  </a>
 							  <a class="right recommended-item-control" href="#recommended-item-carousel" data-slide="next">
 								<i class="fa fa-angle-right"></i>
-							  </a>			 -->
+							  </a>			 
 						</div>
 					</div><!--/recommended_items-->
+
+			
 				</div>
 			</div>
 		</div>
